6cfecf7b12a66ee96bb8d74932aca36483a4d0a1
Jude Bobinihi <boblitejude@gmail.com>
2020-12-16 16:32:47 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        int bae = 9;
+
+            Console.WriteLine("Hello World! their dad");
+            bae+=1;
+            Console.WriteLine("hello bae" + bae);
+            bae +=5;
+            Console.WriteLine(bae);
+
+        }
+    }
+}
